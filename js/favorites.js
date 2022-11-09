57ef263690dbfd5877cb0f55ccb3f8b7e868d272
Paloma Rodrigues Barbosa <palomarodrigues.0013@gmail.com>
2022-11-09 19:02:26 -0300

    :hammer: promises and async await

--- js/favorites.js
+++ js/favorites.js
@@ -23,6 +23,25 @@ export class Favorites {
     this.entries = JSON.parse(localStorage.getItem("@github-favorites:")) || [];
   }
 
+  save() {
+    localStorage.setItem("@github-favorites:", JSON.stringify(this.entries));
+  }
+
+  async add(username) {
+    try {
+      const user = await GithubUser.search(username);
+      if (user.login === undefined) {
+        throw new Error("Usuário não encontrado!");
+      }
+
+      this.entries = [user, ...this.entries];
+      this.update();
+      this.save();
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
   delete(user) {
     const filteredEntries = this.entries.filter(
       (entry) => entry.login !== user.login
@@ -30,6 +49,7 @@ export class Favorites {
 
     this.entries = filteredEntries;
     this.update();
+    this.save();
   }
 }
 
@@ -40,6 +60,16 @@ export class FavoritesView extends Favorites {
     this.tbody = this.root.querySelector("table tbody");
 
     this.update();
+    this.onadd();
+  }
+
+  onadd() {
+    const addButton = this.root.querySelector(".search button");
+    addButton.onclick = () => {
+      const { value } = this.root.querySelector(".search input");
+
+      this.add(value);
+    };
   }
 
   update() {
